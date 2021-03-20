e67497d57ee4fab22fb6f2b013c68b9201f693db
aman679 <amankumar679@gmail.com>
2021-03-21 03:10:02 +0530

    first commit

---
+++ ppmapp/src/main/java/com/aman/ppmapp/PpmappApplication.java
@@ -0,0 +1,13 @@
+package com.aman.ppmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmappApplication.class, args);
+	}
+
+}
